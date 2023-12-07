e5a9588b8a8314ed5d33f71fe6abdcb0f3287ea2
Zhiyuan Liu <skelviper@hotmail.com>
2023-12-07 19:25:27 +0800

    add normal nextera libprep

--- libprep.py
+++ libprep.py
@@ -1,200 +1,210 @@
+"""
+Automated nextera library prep protocol
+@Author: zliu
+@Version: 0.1
+@Date: 2023-11-15
+"""
+
 from opentrons import protocol_api
+# import opentrons.execute # in jupyter
+import time
+import json
+import math
+import os
+import subprocess
+
+################CHARM library prep configuration################
+#malbac_product_concentration_columns = [55,55,55,55,55,55,55,55,55,55,55,55]
+malbac_product_concentration_columns = [55 for i in range(12)]
+if_dry_run = True
+################End CHARM library prep configuration############
+
+# define constant
+AUDIO_FILE_PATH = '/etc/audio/reminder_tone.mp3' 
+
+
+# define custom functions
+def run_quiet_process(command): 
+    subprocess.check_output('{} &> /dev/null'.format(command), shell=True) 
+
+def speaker(): 
+    print('Speaker') 
+    print('Next\t--> CTRL-C')
+    try:
+        run_quiet_process('mpg123 {}'.format(AUDIO_FILE_PATH))
+    except KeyboardInterrupt:
+        print("cancel")
+
+def _pick_up(pipette):
+    try:
+        pipette.pick_up_tip()
+    except protocol_api.labware.OutOfTipsError:
+        for _ in range(8):
+            protocol.set_rail_lights(not protocol.rail_lights_on)
+            if protocol.rail_lights_on:
+                speaker()
+            protocol.delay(seconds=0.2)
+        protocol.pause("Replace empty tip racks")
+        pipette.reset_tipracks()
+        pipette.pick_up_tip()
+    
 
 metadata = {
-    'protocolName': 'Single cell library prep protocol for PKU',
-    'author': 'FAE <fae@opentrons.com>',
-    'source': 'Peking University',
-    'apiLevel': '2.14'   # CHECK IF YOUR API LEVEL HERE IS UP TO DATE
-                         # IN SECTION 5.2 OF THE APIV2 "VERSIONING"
+    'protocolName': 'Automated nextera library prep protocol',
+    'author': 'zliu <skelviper@hotmail.com>',
+    'apiLevel': '2.13' 
 }
 
+if if_dry_run:
+    col_num = 3
+else:
+    col_num = 12
 
-def run(ctx: protocol_api.ProtocolContext):
+def run(protocol: protocol_api.ProtocolContext):
+    if not protocol.rail_lights_on:
+        protocol.set_rail_lights(True)
+    protocol.home()
 
-    # load modules
-    tc_mod = ctx.load_module(module_name='thermocyclerModuleV2')
-    #hs_mod = ctx.load_module(module_name='heaterShakerModuleV1',location="1")
-    # load labware
-    tc_plate = tc_mod.load_labware(name='armadillo_96_wellplate_200ul_pcr_full_skirt')
-    #reagent_plate = hs_mod.load_labware(name='opentrons_96_pcr_adapter_armadillo_wellplate_200ul')
-    reagent_plate = ctx.load_labware('armadillo_96_wellplate_200ul_pcr_full_skirt','4')
-    index_plate = ctx.load_labware('armadillo_96_wellplate_200ul_pcr_full_skirt','1')
-    # load tipracks
-    tipracks_20 = [ctx.load_labware('opentrons_96_tiprack_20ul', s) for s in ['2','3','5','6','9']]
-    
+    malbac_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='3')
+    reagent_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='9')
+    dilute_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='6')
+    pcr_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='2')
+    tipracks = [protocol.load_labware('axygen_96_diytiprack_10ul',location=s) for s in ['1','4','5','7','8','10','11']]
+
     # load instrument
-    m20 = ctx.load_instrument('p20_multi_gen2','right',tip_racks=tipracks_20)
-    # pipette functions   # INCLUDE ANY BINDING TO CLASS
-    '''
-    def pick_up(pipette):
-        try:
-            pipette.pick_up_tip()
-        except protocol_api.labware.OutOfTipsError:
-            ctx.pause("Replace empty tip racks")
-            pipette.reset_tipracks()
-            pipette.pick_up_tip()
-    '''
+    pipette = protocol.load_instrument('p20_multi_gen2', 'right', tip_racks=tipracks)
+
+    # set flow rate for small volume
+    pipette.flow_rate.aspirate = 5  
+    pipette.flow_rate.dispense = 5
     
-
-
-    # helper functions
-
-
-    # reagents
-    TranspositionMix = reagent_plate.wells_by_name()['A1']
-    MALBACProducts = reagent_plate.wells_by_name()['A2']
+    # def in reagent_plate
+    # water, transposition mix, SDS, PCR mix
+    water = reagent_plate.wells_by_name()['A1']
+    TranspositionMix = reagent_plate.wells_by_name()['A2']
     SDS = reagent_plate.wells_by_name()['A3']
     PCRMix = reagent_plate.wells_by_name()['A4']
-    Index = index_plate.wells()
 
-    # plate, tube rack maps
+    # input water volumes for malbac products dialution to 5ng/ul, 
+    # for example, original concentration is 40 ng/ul, load 2 ul of malbac products and 14 ul of water
 
-    # protocol
-    if not ctx.rail_lights_on:
-        ctx.set_rail_lights(True)
+    final_concentration = 5
+    malbac_product_volume = 2
+    water_volume = [malbac_product_volume/final_concentration*concentration-malbac_product_volume for concentration in malbac_product_concentration_columns]
 
-    tc_mod.open_lid()
-    #hs_mod.close_labware_latch()
+    # transfer water to dilute plate
+    _pick_up(pipette)
+    for i in range(col_num):
+        pipette.aspirate(water_volume[i], water.bottom()) 
+        pipette.dispense(water_volume[i], dilute_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.move_to(dilute_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+    pipette.drop_tip()
 
-    # Transfer 3ul Transposition Mix to sample plate on Thermocycler
-    m20.transfer(
-        volume = 3,
-        source = TranspositionMix,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        mix_after = (3, 5),
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
+    # transfer TranspositionMix to pcr plate
+    TranspositionMix_volume = 3
+    _pick_up(pipette)
+    for i in range(col_num):
+        pipette.aspirate(TranspositionMix_volume, TranspositionMix.bottom())
+        pipette.dispense(TranspositionMix_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+    pipette.drop_tip()
 
-    # Transfer 2ul MALBAC products to sample plate on Thermocycler
-    m20.transfer(
-        volume = 2,
-        source = MALBACProducts,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        mix_after = (5, 7),
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
+    # transfer malbac products to dilute plate, mix, and transfer to pcr plate
+    for i in range(col_num):
+        _pick_up(pipette)
+        pipette.aspirate(malbac_product_volume, malbac_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.dispense(malbac_product_volume, dilute_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.mix(10, 16,rate=10,location = dilute_plate.columns_by_name()[str(i+1)][0].bottom(1.5))
+        pipette.aspirate(malbac_product_volume, dilute_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.dispense(malbac_product_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.mix(10,4,rate=10, location = pcr_plate.columns_by_name()[str(i+1)][0].bottom(1.5))
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+        pipette.drop_tip()
 
-    # 55C Incubate for 10min
+    # Pause for Tn5 reaction
+    for _ in range(8):
+        protocol.set_rail_lights(not protocol.rail_lights_on)
+        if protocol.rail_lights_on:
+            speaker()
+        protocol.delay(seconds=0.2)
+    protocol.pause('Pause and transfer PCR plate to thermocycler for Tn5 reaction')
 
-    tc_mod.close_lid()
-    tc_mod.set_lid_temperature(temperature=65)
-    tc_mod.set_block_temperature(
-        temperature=55,
-        hold_time_minutes=10,
-        block_max_volume=10
-    )
-    tc_mod.set_block_temperature(
-        temperature=4,
-        hold_time_seconds=30,
-        block_max_volume=10
-    )
-    tc_mod.deactivate_lid()
-    tc_mod.deactivate_block()
-    tc_mod.open_lid()
+    # transfer SDS to pcr plate and split the library into two plates(Hi-C & Enrich)
+    SDS_volume = 1.25
+    lib_volume = 6.25
 
-    # Transfer 2ul SDS  to sample plate on Thermocycler
-    m20.transfer(
-        volume = 1.3,
-        source = SDS,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        mix_after = (5, 8),
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
-
-    # Incubate 10min at room temperature
-    ctx.delay(minutes=10)
-
-    # Transfer 2ul i5  to sample plate on Thermocycler
-    m20.transfer(
-        volume = 2,
-        source = Index,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
-
-    #Transfer 11.7ul PCRMix to sample plate on Thermocycler
-    m20.transfer(
-        volume = 11.7,
-        source = PCRMix,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        mix_after = (5, 16),
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
-
-    tc_mod.close_lid()
-    tc_mod.set_lid_temperature(temperature=105)
-
-    tc_mod.set_block_temperature(
-        temperature=72,
-        hold_time_minutes=3,
-        block_max_volume=20
-    )
-    tc_mod.set_block_temperature(
-        temperature=98,
-        hold_time_seconds=30,
-        block_max_volume=20
-    )
-    profile = [
-        {'temperature':98, 'hold_time_seconds':15},
-        {'temperature':60, 'hold_time_seconds':30},
-        {'temperature':72, 'hold_time_minutes':2}
-    ]
-    tc_mod.execute_profile(steps=profile, repetitions=5, block_max_volume=20)
-    tc_mod.set_block_temperature(
-        temperature=4,
-        hold_time_seconds=30,
-        block_max_volume=20
-    )
-    tc_mod.open_lid()
+    for i in range(col_num):
+        _pick_up(pipette)
+        pipette.aspirate(SDS_volume, SDS.bottom())
+        pipette.dispense(SDS_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.mix(10, 5,rate=10, location = pcr_plate.columns_by_name()[str(i+1)][0].bottom(1.5))
+        pipette.aspirate(lib_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+        pipette.drop_tip()
     
+    # replace 3 and 6 with i5/i7 index , while SDS reaction
+    for _ in range(8):
+        protocol.set_rail_lights(not protocol.rail_lights_on)
+        if protocol.rail_lights_on:
+            speaker()
+        protocol.delay(seconds=0.2)
+    protocol.pause('Please replace 3 and 6 with i5/i7 index, while SDS reaction')
+    # incubate at RT for 10 min
+    # protocol.delay(minutes=10)
 
-    ctx.pause("Replace empty tip racks")
-    m20.reset_tipracks()
+    del protocol.deck['3']
+    del protocol.deck['6']
+    # with open('./xinglab_pcr96well_semiskirt_280ul.json') as labware_file:
+    #     labware_def = json.load(labware_file)
+    #     i5_plate = protocol.load_labware_from_definition(labware_def,location='6')
+    #     i7_plate = protocol.load_labware_from_definition(labware_def,location='3')
+    i5_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='6')
+    i7_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='3')
 
-    # Transfer 2ul i5  to sample plate on Thermocycler
-    m20.transfer(
-        volume = 2,
-        source = Index,
-        dest = tc_plate.wells(),
-        new_tip = "always",
-        mix_after = (5, 20),
-        blow_out=True,  # required to set location
-        blowout_location="destination well",
-    )
+    # transfer i5 index to pcr plate,
+    # split pcr plate into two plates, one for Hi-C library, one for MALBAC library
+    # for Hi-C library
+    i5_volume = 2
+    i7_volume = 2
+    for i in range(col_num):
+        _pick_up(pipette)
+        pipette.aspirate(i5_volume, i5_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.dispense(i5_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        #pipette.mix(10, 6,rate=10)
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+        pipette.drop_tip()
+    for i in range(col_num):
+        _pick_up(pipette)
+        pipette.aspirate(i7_volume, i7_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.dispense(i7_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        #pipette.mix(10, 8,rate=10)
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+        pipette.drop_tip()
 
-    tc_mod.close_lid()
-    profile = [
-        {'temperature':98, 'hold_time_seconds':30},
-        {'temperature':60, 'hold_time_seconds':30},
-        {'temperature':72, 'hold_time_minutes':2}
-    ]
-    tc_mod.execute_profile(steps=profile, repetitions=5, block_max_volume=22)
+    # transfer PCR mix to pcr plate
+    PCRMix_volume = 9.75
+    for i in range(col_num):
+        _pick_up(pipette)
+        pipette.aspirate(PCRMix_volume, PCRMix.bottom())
+        pipette.dispense(PCRMix_volume, pcr_plate.columns_by_name()[str(i+1)][0].bottom())
+        pipette.mix(10, 15,rate=10, location = pcr_plate.columns_by_name()[str(i+1)][0].bottom(1.5))
+        pipette.move_to(pcr_plate.columns_by_name()[str(i+1)][0].bottom(20))
+        pipette.blow_out()
+        pipette.drop_tip()
 
-    tc_mod.set_block_temperature(
-        temperature=72,
-        hold_time_minutes=5,
-        block_max_volume=22
-    )
+    # Pause for library amplification
+    for _ in range(8):
+        protocol.set_rail_lights(not protocol.rail_lights_on)
+        if protocol.rail_lights_on:
+            speaker()
+        protocol.delay(seconds=0.2)
+    protocol.pause('Pause. 1. Transfer PCR plate to thermocycler for library amplification. 2. replace i5/i7 index for enrich lib.')
 
-    tc_mod.set_block_temperature(
-        temperature=4,
-        hold_time_seconds=30,
-        block_max_volume=22
-    )
-
-    tc_mod.open_lid()
+    protocol.comment('Protocol complete!')
     
-    ctx.pause("Run Completed, Please take the final product to next step, then click resume button!")
-    tc_mod.close_lid()
-    tc_mod.deactivate_lid()
-    tc_mod.deactivate_block()
