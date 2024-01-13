64367c4d0e275c7a1e59b7afb3a584609d552ef2
Zhiyuan Liu <skelviper@hotmail.com>
2024-01-13 22:48:16 +0800

    Refactor pick_up function and remove unnecessary code

--- libprep.charm.py
+++ libprep.charm.py
@@ -30,22 +30,6 @@ def speaker():
     except KeyboardInterrupt:
         print("cancel")
 
-def _pick_up(pipette):
-    """
-    pick up tip, if no tip available, pause and wait for tip replacement
-    """
-    try:
-        pipette.pick_up_tip()
-    except protocol_api.labware.OutOfTipsError:
-        for _ in range(8):
-            protocol.set_rail_lights(not protocol.rail_lights_on)
-            if protocol.rail_lights_on:
-                speaker()
-            protocol.delay(seconds=0.2)
-        protocol.pause("Replace empty tip racks")
-        pipette.reset_tipracks()
-        pipette.pick_up_tip()
-
 def _calc_height(volume, diameter=5.5, bottom_offset=0.3):
     """
     calculate liquid height in well.
@@ -55,13 +39,14 @@ def _calc_height(volume, diameter=5.5, bottom_offset=0.3):
     height_cone = 0
     height_cylinder = 0
 
-    if volume <= 71.53:
-        height_cone = volume / (math.pi * (diameter / 2) ** 2) * 3 
-    else:
-        volume -= 71.53
-        height_cylinder = volume / (math.pi * (diameter / 2) ** 2)
+    #if volume <= 100:
+    #    height_cone = volume / (math.pi * (diameter / 2) ** 2) * 3 
+    #else:
+    #    volume -= 100
+    height_cylinder = volume / (math.pi * (diameter / 2) ** 2)
 
-    height = height_cone + height_cylinder
+    #height = height_cone + height_cylinder
+    height = height_cylinder
     if height < 0:
         return 0 + bottom_offset
     else:
@@ -88,6 +73,22 @@ def run(protocol: protocol_api.ProtocolContext):
     if not protocol.rail_lights_on:
         protocol.set_rail_lights(True)
     protocol.home()
+
+    def _pick_up(pipette):
+    """
+    pick up tip, if no tip available, pause and wait for tip replacement
+    """
+    try:
+        pipette.pick_up_tip()
+    except protocol_api.labware.OutOfTipsError:
+        for _ in range(8):
+            protocol.set_rail_lights(not protocol.rail_lights_on)
+            if protocol.rail_lights_on:
+                speaker()
+            protocol.delay(seconds=0.2)
+        protocol.pause("Replace empty tip racks")
+        pipette.reset_tipracks()
+        pipette.pick_up_tip()
 
     malbac_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='6')
     reagent_plate = protocol.load_labware('xinglab_8stripetube',location='9')
@@ -171,16 +172,13 @@ def run(protocol: protocol_api.ProtocolContext):
         if protocol.rail_lights_on:
             speaker()
         protocol.delay(seconds=0.2)
-    protocol.pause('Please replace 3 and 6 with i5/i7 index, while SDS reaction')
+    protocol.pause('Please replace 3 and 6 with i5/i7 index, while SDS reaction (Set timer manually for 10 min)')
     # incubate at RT for 10 min
     # protocol.delay(minutes=10)
 
     del protocol.deck['3']
     del protocol.deck['6']
-    # with open('./xinglab_pcr96well_semiskirt_280ul.json') as labware_file:
-    #     labware_def = json.load(labware_file)
-    #     i5_plate = protocol.load_labware_from_definition(labware_def,location='6')
-    #     i7_plate = protocol.load_labware_from_definition(labware_def,location='3')
+
     i5_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='6')
     i7_plate = protocol.load_labware('pcr96well_nonskirt_280ul',location='3')
 
